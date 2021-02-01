ea6f60e9a7b036b1e8f33a0130eeeea3b933298e
Robertas Ivanavičius <robertas.i@sanitex.eu>
2021-02-01 10:07:39 +0200

    Dev

--- app/src/main/java/com/ivanavicius/robertas/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/ivanavicius/robertas/barcodescanner/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
